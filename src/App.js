5088d9fc8327738e12f66360b61cef964bf44235
Hsuan <feeng10914017>
2021-01-04 10:49:11 +0800

    update coponents MainContent padding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
+
+//排版用元件 全部都使用
 import MyNavbar from './components/MyNavbar'
 import MyFooter from './components/MyFooter'
 import MainContent from './components/MainContent'
 
-
+//頁面元件
 
 function App() {
   return<>
