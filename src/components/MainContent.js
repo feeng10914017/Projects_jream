5db26ca1f15a1c16ba233350db4cef2ff28287ce
Hsuan <feeng10914017>
2021-01-04 21:16:02 +0800

    update order and something style

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
-
+import Container from 'react-bootstrap/Container'
 function MainContent(props) {
   return (
     <>
       <main role="main" className="flex-shrink-0">
-        <div className="container">{props.children}</div>
+        <Container>{props.children}</Container>
+        {/* <div className="container"></div> */}
       </main>
     </>
   )
--- src/pages/order/Order.js
+++ src/pages/order/Order.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
 import Card from 'react-bootstrap/Card'
 import Nav from 'react-bootstrap/Nav'
 import Button from 'react-bootstrap/Button'
 import LeaseImg from './images/Leoncino_250_gray.jpg'
-import Table from 'react-bootstrap/Table'
 
-import './order.scss'
+import './orderStepOne.scss'
 import OrderStep from './OrderStep'
 function Order() {
   return (
@@ -27,43 +29,46 @@ function Order() {
           </Nav>
         </Card.Header>
         <Card.Body id="motor">
-          <Table responsive>
-            <thead>
-              <tr>
-                <th class="figu">
+          <div class="lease">
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="" />
+            </Form.Group>
+            <div class="cardContent">
+              <Row>
+                <div class="col-3 motorImage">
                   <img src={LeaseImg} />
-                </th>
-                <th>SUZUKI</th>
-                <th>GSX - S1000S</th>
-                <th>BROWN</th>
-                <th>$1000</th>
-              </tr>
-            </thead>
-          </Table>
-          {/* <div class="cardTitle">
-            <Card.Title>
-              <p>SUZUKI</p>
-              <p>GSX - S1000S</p>
-              <p>BROWN</p>
-            </Card.Title>
-          </div> */}
-          <hr />
-          <Card.Text>
-            With supporting text below as a natural lead-in to additional
-            content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-
-        <Card.Body id="produc">
-          <Card.Title>Special title treatment</Card.Title>
-          <Card.Text>
-            With supporting text below as a natural lead-in to additional
-            content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+                </div>
+                <p class="col-2">SUZUKI</p>
+                <p class="col-2">GSX - S1000S</p>
+                <p class="col-2">BROWN</p>
+                <h6 class="col-3">$1000</h6>
+              </Row>
+              <hr />
+              <Row>
+                <p class="col-3">取車地點</p>
+                <p class="col-3">台北濱江店</p>
+                <p class="col-3">還車地點</p>
+                <p class="col-3">台北濱江店</p>
+              </Row>
+              <Row>
+                <p class="col-3">取車日期</p>
+                <p class="col-3">2020/12/19</p>
+                <p class="col-3">還車日期</p>
+                <p class="col-3">2020/12/20</p>
+              </Row>
+              <Row>
+                <p class="col-3">取車時間</p>
+                <p class="col-3">13:15</p>
+                <p class="col-3">還車時間</p>
+                <p class="col-3">09:15</p>
+              </Row>
+            </div>
+          </div>
         </Card.Body>
       </Card>
+      <Button className="test" variant="primary">
+        Go somewhere
+      </Button>
     </>
   )
 }
