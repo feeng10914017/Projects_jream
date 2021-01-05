6cf7e3ead43f2359e2f432da5a96181fc2aa8542
Hsuan <feeng10914017>
2021-01-05 12:46:28 +0800

    update order step one

--- src/pages/order/Order.js
+++ src/pages/order/Order.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
 import Card from 'react-bootstrap/Card'
 import Nav from 'react-bootstrap/Nav'
 import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
 import LeaseImg from './images/Leoncino_250_gray.jpg'
+import LeaseImg1 from './images/7418807_R-removebg-preview.png'
 
 import './orderStepOne.scss'
 import OrderStep from './components/OrderStep'
@@ -28,6 +31,7 @@ function Order() {
             </Nav.Item>
           </Nav>
         </Card.Header>
+
         <Card.Body id="motor">
           <div class="lease">
             <Form.Group controlId="formBasicCheckbox">
@@ -65,10 +69,47 @@ function Order() {
             </div>
           </div>
         </Card.Body>
+
+        <Card.Body id="product">
+          <div class="lease">
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="" />
+            </Form.Group>
+            <div class="cardContent">
+              <Row>
+                <div class="col-2 motorImage">
+                  <img src={LeaseImg1} />
+                </div>
+                <p class="col-4">
+                  BENKIA 摩托車防水秋冬防摔衣 黑 女款 騎行服 防摔衣 秋冬款
+                  HDF-JD-W149
+                </p>
+                <p class="col-2">
+                  <Form.Group as={Col} controlId="formGridState">
+                    <Form.Label></Form.Label>
+                    <Form.Control as="select" defaultValue="Choose...">
+                      <option>xxl</option>
+                      <option>...</option>
+                    </Form.Control>
+                  </Form.Group>
+                </p>
+                <p class="col-2">
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label></Form.Label>
+                    <Form.Control type="number" placeholder="1" />
+                  </Form.Group>
+                </p>
+                <h6 class="col-2">$1000</h6>
+              </Row>
+            </div>
+          </div>
+        </Card.Body>
       </Card>
-      <Button className="test" variant="primary">
-        Go somewhere
-      </Button>
+      <Row className="justify-content-center">
+        <Button className="next place-center" variant="primary">
+          下一步
+        </Button>
+      </Row>
     </>
   )
 }
