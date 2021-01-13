d7b6ce5fb81321ce3f1e46a8f0bde03da5df949b
AqingOlin <73264755+AqingOlin@users.noreply.github.com>
2021-01-13 14:56:28 +0800

    登入頁面Update 0113

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -17,22 +17,22 @@ function MyNavbar(props) {
   const { location } = props
   // console.log(location.pathname)
 
-  useEffect(() => {
-    let navbarPosition = $('.bannerHeader').offset().top
-    // console.log(navbarPosition)
-    $(window).on('scroll', function () {
-      let scrollTop = $(this).scrollTop()
-      console.log(scrollTop)
-      if (scrollTop >= navbarPosition) {
-        $('.navbar').addClass('active')
-        $('.logo').attr('src', 'http://localhost:3000/images/logo/logo_w.png')
-        // console.log('+')
-      } else {
-        $('.navbar').removeClass('active')
-        // console.log('-')
-      }
-    })
-  }, [])
+  // useEffect(() => {
+  //   let navbarPosition = $('.bannerHeader').offset().top
+  //   // console.log(navbarPosition)
+  //   $(window).on('scroll', function () {
+  //     let scrollTop = $(this).scrollTop()
+  //     console.log(scrollTop)
+  //     if (scrollTop >= navbarPosition) {
+  //       $('.navbar').addClass('active')
+  //       $('.logo').attr('src', 'http://localhost:3000/images/logo/logo_w.png')
+  //       // console.log('+')
+  //     } else {
+  //       $('.navbar').removeClass('active')
+  //       // console.log('-')
+  //     }
+  //   })
+  // }, [])
 
   //一般header
   const display = (
@@ -43,7 +43,7 @@ function MyNavbar(props) {
           expand="lg"
           bg="primary"
           variant="dark"
-        // fixed="top"
+          // fixed="top"
         >
           <Navbar.Brand href="#home">
             <Nav.Link as={NavLink} to="/">
@@ -87,52 +87,47 @@ function MyNavbar(props) {
   )
 
   const homeDisplay = (
-    <><header className="bannerHeader">
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        variant="dark"
-        fixed="top"
-
-      >
-        <Navbar.Brand href="#home">
-          <Nav.Link as={NavLink} to="/">
-            <img
-              className="logo"
-              src="http://localhost:3000/images/logo/logo_r.png"
-              alt=""
-            />
-          </Nav.Link>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link as={NavLink} to="/news">
-              <h6>最新消息</h6>
+    <>
+      <header className="bannerHeader">
+        <Navbar collapseOnSelect expand="lg" variant="dark" fixed="top">
+          <Navbar.Brand href="#home">
+            <Nav.Link as={NavLink} to="/">
+              <img
+                className="logo"
+                src="http://localhost:3000/images/logo/logo_r.png"
+                alt=""
+              />
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/motor">
-              <h6>機車租賃</h6>
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/product">
-              <h6>周邊商品</h6>
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/location">
-              <h6>據點消息</h6>
-            </Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link as={NavLink} to="/login">
-              <IoPersonOutline size="18" />
-              <h6>登入</h6>
-            </Nav.Link>
-            <Nav.Link eventKey={2} as={NavLink} to="/order">
-              <IoCartOutline size="20" />
-              <h6>購物車</h6>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </header>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link as={NavLink} to="/news">
+                <h6>最新消息</h6>
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/motor">
+                <h6>機車租賃</h6>
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/product">
+                <h6>周邊商品</h6>
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/location">
+                <h6>據點消息</h6>
+              </Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link as={NavLink} to="/login">
+                <IoPersonOutline size="18" />
+                <h6>登入</h6>
+              </Nav.Link>
+              <Nav.Link eventKey={2} as={NavLink} to="/order">
+                <IoCartOutline size="20" />
+                <h6>購物車</h6>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </header>
     </>
   )
   return <>{location.pathname == '/' ? homeDisplay : display}</>
--- src/pages/member/Login.js
+++ src/pages/member/Login.js
@@ -3,40 +3,111 @@ import { Link } from 'react-router-dom'
 import { Form, Button, Image } from 'react-bootstrap'
 
 import './member.scss'
+import './login.scss'
 
-var background = { backgroundSize: 'cover' }
+// var background = { backgroundSize: 'cover' }
 
 function Login(props) {
+  useEffect(() => {
+    document.querySelector('.img__btn').addEventListener('click', function () {
+      document.querySelector('.cont').classList.toggle('s--signup')
+    })
+  })
   return (
-    <div className="A-login">
-      <div style={{ width: 'auto', position: 'relative' }}>
-        <Button className="A-SIG">圖片文字</Button>
-        <Image
-          style={background}
-          responsive
-          src="http://localhost:3000/images/home/images2.jpg"
-        ></Image>
+    <>
+      {/* <p className="tip">Click on button in image container</p> */}
+      <div className="cont">
+        <div className="form sign-in">
+          <h2>Welcome back,</h2>
+          <label>
+            <span>Account</span>
+            <input type="account" />
+          </label>
+          <label>
+            <span>Password</span>
+            <input type="password" />
+          </label>
+          <p className="forgot-pass">Forgot password?</p>
+          <Link to="./Member">
+            <button type="button" className="submit A-Btn">
+              Sign In
+            </button>
+          </Link>
+          <button type="button" className="fb-btn A-Btn">
+            Connect with <span>facebook</span>
+          </button>
+          <p class="mt-5 mb-3 text-muted text-center">&copy; 2020-2021</p>
+        </div>
+        <div className="sub-cont">
+          <div className="img">
+            <div className="img__text m--up">
+              <h2>New here?</h2>
+              <p>Sign up and discover great amount of new opportunities!</p>
+            </div>
+            <div className="img__text m--in">
+              <h2>One of us?</h2>
+              <p>
+                If you already has an account, just sign in. We've missed you!
+              </p>
+            </div>
+            <div className="img__btn">
+              <span className="m--up">Sign Up</span>
+              <span className="m--in">Sign In</span>
+            </div>
+          </div>
+          <div className="form sign-up">
+            <h2>Time to feel like home,</h2>
+            <label>
+              <span>Name</span>
+              <input type="text" />
+            </label>
+            <label>
+              <span>Account</span>
+              <input type="account" />
+            </label>
+            <label>
+              <span>Password</span>
+              <input type="password" />
+            </label>
+            <button type="button" className="submit A-Btn">
+              Sign Up
+            </button>
+            <button type="button" className="fb-btn A-Btn">
+              Join with <span>facebook</span>
+            </button>
+          </div>
+        </div>
       </div>
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>帳號:</Form.Label>
-          <Form.Control type="email" placeholder="Account" />
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>密碼:</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="保持登入" />
-        </Form.Group>
-        <Link to="/member">
-          <Button variant="primary" type="submit">
-            登入
-          </Button>
-        </Link>
-        <p class="mt-5 mb-3 text-muted text-center">&copy; 2020-2021</p>
-      </Form>
-    </div>
+    </>
+    // <div className="A-login">
+    //   <div style={{ width: 'auto', position: 'relative' }}>
+    //     <Button className="A-SIG">圖片文字</Button>
+    //     <Image
+    //       style={background}
+    //       responsive
+    //       src="http://localhost:3000/images/home/images2.jpg"
+    //     ></Image>
+    //   </div>
+    //   <Form>
+    //     <Form.Group controlId="formBasicEmail">
+    //       <Form.Label>帳號:</Form.Label>
+    //       <Form.Control type="email" placeholder="Account" />
+    //     </Form.Group>
+    //     <Form.Group controlId="formBasicPassword">
+    //       <Form.Label>密碼:</Form.Label>
+    //       <Form.Control type="password" placeholder="Password" />
+    //     </Form.Group>
+    //     <Form.Group controlId="formBasicCheckbox">
+    //       <Form.Check type="checkbox" label="保持登入" />
+    //     </Form.Group>
+    //     <Link to="/member">
+    //       <Button variant="primary" type="submit">
+    //         登入
+    //       </Button>
+    //     </Link>
+    //     <p class="mt-5 mb-3 text-muted text-center">&copy; 2020-2021</p>
+    //   </Form>
+    // </div>
   )
 }
 
