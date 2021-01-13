b69cc9216534fd89361bdc2f886f804b77fdd43f
AqingOlin <73264755+AqingOlin@users.noreply.github.com>
2021-01-13 16:07:38 +0800

    update0113

--- src/pages/member/Login.js
+++ src/pages/member/Login.js
@@ -1,11 +1,8 @@
 import { React, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Form, Button, Image } from 'react-bootstrap'
 
 import './member.scss'
 import './login.scss'
-
-// var background = { backgroundSize: 'cover' }
 
 function Login(props) {
   useEffect(() => {
@@ -79,35 +76,6 @@ function Login(props) {
         </div>
       </div>
     </>
-    // <div className="A-login">
-    //   <div style={{ width: 'auto', position: 'relative' }}>
-    //     <Button className="A-SIG">圖片文字</Button>
-    //     <Image
-    //       style={background}
-    //       responsive
-    //       src="http://localhost:3000/images/home/images2.jpg"
-    //     ></Image>
-    //   </div>
-    //   <Form>
-    //     <Form.Group controlId="formBasicEmail">
-    //       <Form.Label>帳號:</Form.Label>
-    //       <Form.Control type="email" placeholder="Account" />
-    //     </Form.Group>
-    //     <Form.Group controlId="formBasicPassword">
-    //       <Form.Label>密碼:</Form.Label>
-    //       <Form.Control type="password" placeholder="Password" />
-    //     </Form.Group>
-    //     <Form.Group controlId="formBasicCheckbox">
-    //       <Form.Check type="checkbox" label="保持登入" />
-    //     </Form.Group>
-    //     <Link to="/member">
-    //       <Button variant="primary" type="submit">
-    //         登入
-    //       </Button>
-    //     </Link>
-    //     <p class="mt-5 mb-3 text-muted text-center">&copy; 2020-2021</p>
-    //   </Form>
-    // </div>
   )
 }
 
