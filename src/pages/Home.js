de89aa66d19d97c50cea819f4225fc6cddd9c17b
Hsuan <feeng10914017>
2021-01-14 15:10:56 +0800

    update home page well.png

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,19 @@ import React, { useState, useEffect } from 'react'
 import { Parallax } from 'react-parallax'
 import { Container, Row, Col, Card, Button, Form } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import $ from 'jquery'
+
+//import video
 import Banner from '../components/video/homeBanner.mp4'
+
+//bootstrap-icon
+import { GiSelect } from 'react-icons/gi'
+import { RiMotorbikeFill, RiArrowRightSFill } from 'react-icons/ri'
+import { MdDateRange } from 'react-icons/md'
+import { FaStoreAlt } from 'react-icons/fa'
+
+//import style
 import './home.scss'
+
 function Home() {
   // const styles = {
   //   fontFamily: 'sans-serif',
@@ -71,38 +81,105 @@ function Home() {
           </div>
           {/* step */}
           <article className="homeStep">
-            <h4 className="text-center">
-              簡單&nbsp;3&nbsp;步驟&nbsp;&nbsp;&nbsp;即可租車
-            </h4>
-            <section className="step123"></section>
+            <Row>
+              <img src="http://localhost:3000/images/home/stepThreeFingers.svg" />
+              <h4>簡單&nbsp;3&nbsp;步驟&nbsp;&nbsp;&nbsp;即可租車</h4>
+            </Row>
+            <section className="step123">
+              <section>
+                <div className="stepIcon">
+                  <GiSelect size="7.5rem" color="#cacaca" />
+                  <RiMotorbikeFill
+                    size="4rem"
+                    color="#b02825"
+                    className="stepInline"
+                  />
+                </div>
+                <div className="stepCaption">
+                  <div>
+                    <h5>1</h5>
+                  </div>
+                  <h5>先選車</h5>
+                </div>
+              </section>
+              <RiArrowRightSFill size="3rem" color="#cacaca" />
+              <section>
+                <div className="stepIcon">
+                  <GiSelect size="7.5rem" color="#cacaca" />
+                  <MdDateRange
+                    size="4rem"
+                    color="#b02825"
+                    className="stepInline"
+                  />
+                </div>
+                <div className="stepCaption">
+                  <div>
+                    <h5>2</h5>
+                  </div>
+                  <h5>再選日期</h5>
+                </div>
+              </section>
+              <RiArrowRightSFill size="3rem" color="#cacaca" />
+              <section>
+                <div className="stepIcon">
+                  <GiSelect size="7.5rem" color="#cacaca" />
+                  <FaStoreAlt
+                    size="4rem"
+                    color="#b02825"
+                    className="stepInline"
+                  />
+                </div>
+                <div className="stepCaption">
+                  <div>
+                    <h5>3 </h5>
+                  </div>
+                  <h5>最後選取車店家</h5>
+                </div>
+              </section>
+            </section>
           </article>
           <article className="homeBlock hBlock1">
-            <Col md={9}>
-              <Row>
-                <div className="homeLineSL"></div>
-                <h3>RECOMMEND</h3>
-                <div className="homeLineM"></div>
-              </Row>
-              <div className="blockContainer"></div>
-            </Col>
+            <Row>
+              <Col md={8}>
+                <Row>
+                  <div className="homeLineSL"></div>
+                  <h3>RECOMMEND</h3>
+                  <div className="homeLineM"></div>
+                </Row>
+                <div className="blockContainer"></div>
+              </Col>
+              <Col md={{ span: 2, offset: 1 }} className="well1">
+                <div>
+                  <img src="http://localhost:3000/images/home/well.png" />
+                </div>
+              </Col>
+            </Row>
           </article>
         </Container>
       </section>
-      <Parallax bgImage={bgImg1} strength={250} className='zIndex1'>
+
+      <Parallax bgImage={bgImg1} strength={250} className="zIndex1">
         <div style={{ height: 800 }}></div>
       </Parallax>
 
       <section className="homeSquare2">
         <Container>
           <article className="homeBlock">
-            <Col md={{ span: 9, offset: 3 }}>
-              <Row>
-                <div className="homeLineM"></div>
-                <h3>FEATURED</h3>
-                <div className="homeLineSR"></div>
-              </Row>
-              <div className="blockContainer"></div>
-            </Col>
+            <Row>
+              <Col md={4} className="well2">
+                <div>
+                  <img src="http://localhost:3000/images/home/well.png" />
+                </div>
+              </Col>
+              <Col md={8}>
+                <Row>
+                  <div className="homeLineM"></div>
+                  <h3>FEATURED</h3>
+                  <div className="homeLineSR"></div>
+                </Row>
+                <div className="blockContainer"></div>
+              </Col>
+            </Row>
           </article>
 
           {/* 關於我們 */}
@@ -114,31 +191,41 @@ function Home() {
                 <div className="homeLineM"></div>
               </Row>
               <div className="blockContainer">
-                <h6>現代人工作壓力大，有些人會選擇在假日騎車或是開車出遊，在這當中有許多人會選擇騎乘重機出遊，藉由騎乘的過程來釋放平時的壓力。</h6>
+                <h6>
+                  現代人工作壓力大，有些人會選擇在假日騎車或是開車出遊，在這當中有許多人會選擇騎乘重機出遊，藉由騎乘的過程來釋放平時的壓力。
+                </h6>
                 <br />
-                <h6>但一台重機價格不斐，所以我們希望藉由這個平台，吸引有著重機夢但經濟預算不允許，或是假日想要騎著重機跑跑山路、海線欣賞風景、亦或是享受騎乘時快感的人，有一個值得信任租賃平台讓他/她們去租賃重機。</h6>
+                <h6>
+                  但一台重機價格不斐，所以我們希望藉由這個平台，吸引有著重機夢但經濟預算不允許，或是假日想要騎著重機跑跑山路、海線欣賞風景、亦或是享受騎乘時快感的人，有一個值得信任租賃平台讓他/她們去租賃重機。
+                </h6>
               </div>
             </Col>
           </article>
         </Container>
       </section>
-      <Parallax bgImage={bgImg2} strength={300} className='zIndex1'>
-        <div style={{ height: 800 }}>
 
-        </div>
+      <Parallax bgImage={bgImg2} strength={300} className="zIndex1">
+        <div style={{ height: 800 }}></div>
       </Parallax>
 
       <section className="homeSquare3">
         <Container>
           <article className="homeBlock hBlock1">
-            <Col md={9}>
-              <Row>
-                <div className="homeLineSL"></div>
-                <h3>CONTACT&nbsp;&nbsp;US</h3>
-                <div className="homeLineM"></div>
-              </Row>
-              <div className="blockContainer"></div>
-            </Col>
+            <Row>
+              <Col md={8}>
+                <Row>
+                  <div className="homeLineSL"></div>
+                  <h3>CONTACT&nbsp;&nbsp;US</h3>
+                  <div className="homeLineM"></div>
+                </Row>
+                <div className="blockContainer"></div>
+              </Col>
+              <Col md={{ span: 2, offset: 1 }} className="well3">
+                <div>
+                  <img src="http://localhost:3000/images/home/well.png" />
+                </div>
+              </Col>
+            </Row>
           </article>
         </Container>
       </section>
