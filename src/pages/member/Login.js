37d594fdd0d7a4687b333c5ac6b3e7a6ea97e01b
AqingOlin <73264755+AqingOlin@users.noreply.github.com>
2021-01-16 11:12:38 +0800

    會員頁面整理0116

--- src/pages/member/Login.js
+++ src/pages/member/Login.js
@@ -15,20 +15,20 @@ function Login() {
   // }
   return (
     <>
-      <article className="cont">
-        <div className="form sign-in">
-          <h2>Welcome back,</h2>
+      <div className="cont">
+        <div className="form sign-in A-label">
+          <h2 className="AL-h2">Welcome back,</h2>
           <label>
             <span>Account</span>
-            <input type="account" />
+            <input type="account" className="LI-input" />
           </label>
           <label>
             <span>Password</span>
-            <input type="password" />
+            <input type="password" className="LI-input" />
           </label>
           <p className="forgot-pass">Forgot password?</p>
           <Link to="./Member">
-            <button type="button" className="submit A-Btn">
+            <button type="button" className="submit A-Btn Login-button">
               Sign In
             </button>
           </Link>
@@ -49,21 +49,20 @@ function Login() {
             cookiePolicy={'single_host_origin'}
             isSignedIn={true}
           /> */}
-          <button type="button" className="fb-btn A-Btn">
+          <button type="button" className="fb-btn A-Btn Login-button">
             Connect with <span>facebook</span>
           </button>
 
           <p class="mt-5 mb-3 text-muted text-center">&copy; 2020-2021</p>
         </div>
-
         <div className="sub-cont">
           <div className="img">
             <div className="img__text m--up">
-              <h2>New here?</h2>
+              <h2 className="AL-h2-2">New here?</h2>
               <p>Sign up and discover great amount of new opportunities!</p>
             </div>
             <div className="img__text m--in">
-              <h2>One of us?</h2>
+              <h2 className="AL-h2-2">One of us?</h2>
               <p>
                 If you already has an account, just sign in. We've missed you!
               </p>
@@ -73,29 +72,29 @@ function Login() {
               <span className="m--in">Sign In</span>
             </div>
           </div>
-          <div className="form sign-up">
-            <h2>Time to feel like home,</h2>
+          <div className="form sign-up A-label">
+            <h2 className="AL-h2">Time to feel like home,</h2>
             <label>
               <span>Name</span>
-              <input type="text" />
+              <input type="text" className="LI-input" />
             </label>
             <label>
               <span>Account</span>
-              <input type="account" />
+              <input type="account" className="LI-input" />
             </label>
             <label>
               <span>Password</span>
-              <input type="password" />
+              <input type="password" className="LI-input" />
             </label>
-            <button type="button" className="submit A-Btn">
+            <button type="button" className="submit A-Btn Login-button">
               Sign Up
             </button>
-            <button type="button" className="fb-btn A-Btn">
+            <button type="button" className="fb-btn A-Btn Login-button">
               Join with <span>facebook</span>
             </button>
           </div>
         </div>
-      </article>
+      </div>
     </>
   )
 }
