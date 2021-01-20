5b4d70ca5aacae5eb7454dac9e6b09bdd8770815
kevino430 <73264898+kevino430@users.noreply.github.com>
2021-01-20 23:57:29 +0800

    fixing searching place

--- src/pages/location/Location.js
+++ src/pages/location/Location.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Google from './GoogleAPI'
 import { Button } from 'react-bootstrap'
 import './Location.css'
@@ -15,6 +15,49 @@ function Location() {
     opentime: '',
   })
   const [dataFromLeft, setDataFromLeft] = useState(true)
+  // searching part--------------------------
+  const [searchQuery, setSearchQuery] = useState('')
+  const [doSearch, setDoSearch] = useState(false)
+  const [locations, setLocations] = useState(null)
+  useEffect(() => {
+    setLocations(
+      Locationdata.filter((location, index) => {
+        return (
+          location.name.includes(searchQuery) ||
+          location.address.includes(searchQuery) ||
+          location.phone.includes(searchQuery) ||
+          location.opentime.includes(searchQuery)
+        )
+      }).map((location, index) => {
+        return (
+          <li key={index}>
+            <div className="Location_item">
+              <div className="photoBox">
+                <img src={location.img} alt="Product" />
+              </div>
+              <h5>{location.name}</h5>
+              <div className="address fontSize14">門市地址</div>
+              <div className="time fontSize14">營業時間</div>
+              <div className="phone fontSize14">連絡電話</div>
+              <div className="addressA fontSize14">{location.address}</div>
+              <div className="timeA fontSize14">{location.opentime}</div>
+              <div className="phoneA fontSize14">{location.phone}</div>
+
+              <Button
+                variant="primary"
+                onClick={() => {
+                  setItem(location)
+                  setDataFromLeft(true)
+                }}
+              >
+                詳細資訊
+              </Button>
+            </div>
+          </li>
+        )
+      })
+    )
+  }, [doSearch])
 
   return (
     <div className="d-flex">
@@ -22,7 +65,6 @@ function Location() {
         className="width_1 nopadding border flexSrink_0"
         style={{ height: 'calc(100vh - 80px)' }}
       >
-        {' '}
         <div className="SearchingBox border">
           <Row>
             <ul className="SearchingPlace">
@@ -30,9 +72,18 @@ function Location() {
                 className="searchingInp border "
                 type="text"
                 placeholder="據點搜尋"
+                // ---------searching part--------------------------
+                value={searchQuery}
+                onKeyDown={(event) => {
+                  if (event.keyCode === 13) {
+                    setDoSearch(!doSearch)
+                  }
+                }}
+                onChange={(e) => setSearchQuery(e.target.value)}
               />
+              {/* e is event， target : element， */}
 
-              <Form.Control
+              {/* <Form.Control
                 className="searchingInp border "
                 as="select"
                 defaultValue="Choose"
@@ -40,46 +91,23 @@ function Location() {
                 {Locationdata.map((location, index) => {
                   return <option>{location.city}</option>
                 })}
-              </Form.Control>
+              </Form.Control> */}
             </ul>
-            <Button size="lg" variant="primary" className="SearchingBtn ">
+
+            <Button
+              size="lg"
+              variant="primary"
+              className="SearchingBtn "
+              onClick={() => {
+                setDoSearch(!doSearch)
+              }}
+            >
               Search
             </Button>
           </Row>
         </div>
         <nav className="L_nav-menu ">
-          <ul className="nopadding">
-            {Locationdata.map((location, index) => {
-              return (
-                <li key={index}>
-                  <div className="Location_item">
-                    <div className="photoBox">
-                      <img src={location.img} alt="Product" />
-                    </div>
-                    <h5>{location.name}</h5>
-                    <div className="address fontSize14">門市地址</div>
-                    <div className="time fontSize14">營業時間</div>
-                    <div className="phone fontSize14">連絡電話</div>
-                    <div className="addressA fontSize14">
-                      {location.address}
-                    </div>
-                    <div className="timeA fontSize14">{location.opentime}</div>
-                    <div className="phoneA fontSize14">{location.phone}</div>
-
-                    <Button
-                      variant="primary"
-                      onClick={() => {
-                        setItem(location)
-                        setDataFromLeft(true)
-                      }}
-                    >
-                      詳細資訊
-                    </Button>
-                  </div>
-                </li>
-              )
-            })}
-          </ul>
+          <ul className="nopadding">{locations}</ul>
         </nav>
       </div>
 
