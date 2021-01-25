4d4e9fb4e4ea73b731129d61f273ac92c9da13f5
syuanBrother <feeng10914017@gmail.com>
2021-01-26 06:07:26 +0800

    homePage ContactUs  creat Notification feature

--- src/components/home/ContactUs.js
+++ src/components/home/ContactUs.js
@@ -1,20 +1,49 @@
 import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
-import { Input, Button } from 'antd'
+import { Input, Button, notification, Space } from 'antd'
 
 const { TextArea } = Input
+const openNotificationWithIcon = (type) => {
+  notification[type]({
+    message: 'JREAM',
+    description:
+      '感謝您的來信，您的信息我們已經收到囉，我們會盡快地回覆您，請您耐心等候。',
+  })
+}
+const openNotificationWithIcon1 = (type) => {
+  notification[type]({
+    message: 'JREAM',
+    description:
+      '您還有欄位沒有填哦，快輸入您的姓名與信箱，並把您想說的話告訴我們哦。',
+  })
+}
+notification.config({
+  placement: 'topRight',
+  top: 100,
+  duration: 6,
+})
 class ContactUs extends React.Component {
   state = {
     value: '',
+    value1: '',
+    value2: '',
   }
 
   onChange = ({ target: { value } }) => {
     this.setState({ value })
+    console.log(value)
+  }
+  onChange1 = ({ target: { value1 } }) => {
+    this.setState({ value1 })
+  }
+  onChange2 = ({ target: { value2 } }) => {
+    this.setState({ value2 })
   }
 
   render() {
     const { value } = this.state
-
+    const { value1 } = this.state
+    const { value2 } = this.state
     return (
       <>
         <article className="blockContainer">
@@ -25,13 +54,17 @@ class ContactUs extends React.Component {
                   <Col md={6} xs={12}>
                     <h6>姓名</h6>
                     <Input
-                    // placeholder="Basic usage"
+                      value={value1}
+                      onChange={this.onChange1}
+                      // placeholder="Basic usage"
                     />
                   </Col>
                   <Col md={6} xs={12}>
                     <h6>信箱</h6>
                     <Input
-                    // placeholder="Basic usage"
+                      value={value2}
+                      onChange={this.onChange2}
+                      // placeholder="Basic usage"
                     />
                   </Col>
                 </Row>
@@ -45,7 +78,22 @@ class ContactUs extends React.Component {
                   />
                 </Col>
                 <Col>
-                  <Button block size="large">
+                  <Button
+                    block
+                    size="large"
+                    onClick={() => {
+                      if (value === '' || value1 === '' || value2 === '') {
+                        openNotificationWithIcon1('warning')
+                      } else {
+                        this.setState({
+                          value1: '',
+                          value2: '',
+                          value: '',
+                        })
+                        openNotificationWithIcon('success')
+                      }
+                    }}
+                  >
                     <h6>送出</h6>
                   </Button>
                 </Col>
